4aff258391240f0430c62684c687c566c4551fc0
Sean Macfoy <sean_macfoy@outlook.com>
2023-06-09 21:27:23 +0100

    Implemented addition / scalar multiplication

--- src/Vector2D.cpp
+++ src/Vector2D.cpp
@@ -1,9 +1,39 @@
 #include "Vector2D.hpp"
 
 Vector2D::Vector2D()
+: m_x(0)
+, m_y(0)
+{
+}
+
+Vector2D::Vector2D(int x, int y)
+: m_x(x)
+, m_y(y)
 {
 }
 
 Vector2D::~Vector2D()
 {
+}
+
+Vector2D Vector2D::operator+(Vector2D &v)
+{
+  return Vector2D(m_x + v.m_x, m_y + v.m_y);
+}
+
+Vector2D Vector2D::operator-(Vector2D &v)
+{
+  return Vector2D(m_x - v.m_x, m_y - v.m_y);
+}
+
+Vector2D Vector2D::operator*(double a)
+{
+  return Vector2D(a * m_x, a * m_y);
+}
+
+Vector2D Vector2D::operator/(double a)
+{
+  double b = 1.0 / a;
+
+  return operator*(b);
 }
--- src/Vector2D.hpp
+++ src/Vector2D.hpp
@@ -6,9 +6,28 @@ class Vector2D
 public:
 
   Vector2D();
+  Vector2D(int x, int y);
   ~Vector2D();
 
+  // Accessors
+
+  double x() { return m_x; }
+  double y() { return m_y; }
+
+  void x(double _x) { m_x = _x; }
+  void y(double _y) { m_y = _y; }
+
+  // Operators
+
+  Vector2D operator+(Vector2D &v);
+  Vector2D operator-(Vector2D &v);
+  Vector2D operator*(double a);
+  Vector2D operator/(double a);
+
 private:
+
+  double m_x;
+  double m_y;
 };
 
 #endif
